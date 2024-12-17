a4115037aaf1f757875defb9a92a388bbf60c122
Barış Savak <128433645+bariss30@users.noreply.github.com>
2024-12-17 18:48:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const text = "Hello World 👋 !!!";
+const text = "Hi 👋, I'm Barış Savak";
 let index = 0;
 const typingText = document.getElementById("typing-text");
 
@@ -6,8 +6,30 @@ function type() {
     if (index < text.length) {
         typingText.textContent += text.charAt(index);
         index++;
-        setTimeout(type, 300); // 300 ms bekle
+        setTimeout(type, 75); // 75 ms bekle
+    } else {
+        setTimeout(resetText, 1000); // Yazı bittiğinde 1 saniye bekle ve sıfırla
     }
 }
 
-window.onload = type; 
+function resetText() {
+    typingText.textContent = ""; // Yazıyı temizle
+    index = 0; // Indexi sıfırla
+    type(); // Yazıyı tekrar başlat
+}
+
+window.onload = type;
+
+// "Çalışma Konularım" alanına tıklama işlemi
+document.querySelector("#calismalar").addEventListener("click", () => {
+    const items = document.querySelectorAll(".topic-item");
+    const isHidden = Array.from(items).every(item => !item.classList.contains("show"));
+    
+    items.forEach(item => {
+        if (isHidden) {
+            item.classList.add("show"); // Hepsini göster
+        } else {
+            item.classList.remove("show"); // Hepsini gizle
+        }
+    });
+});
